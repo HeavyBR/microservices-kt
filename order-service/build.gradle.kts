91dce9e235ac7b13dfcf3835b9d1116ec42c4cda
Matheus Cumpian <matheus.cumpian@mercadolivre.com>
2022-12-15 22:57:11 -0300

    initial commit

---
+++ order-service/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("ifood.kotlin-application-conventions")
+}
+
+dependencies {
+
+}
+
+application {
+    // Define the main class for the application.
+    mainClass.set("ifood.order-service.MainKt")
+}
---
+++ order-service/src/main/kotlin/ifood/Main.kt
@@ -0,0 +1,5 @@
+package ifood
+
+fun main() {
+    println("Order Service")
+}
